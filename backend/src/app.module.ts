4f08a3ab4cf19d4c808c4b16cc718c61fa2f0e1e
andres0v13d0 <oviedojonathan2001@gmail.com>
2025-03-20 10:25:27 -0500

    Security

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import *  as AWS from '@aws-sdk/client-secrets-manager';
+import * as fs from 'fs';
+
+async function getDatabaseSecrets() {
+  const client = new AWS.SecretsManagerClient({ region: 'us-east-2' });
+  
+  try{
+    const response = await client.send(
+      new AWS.GetSecretValueCommand({ 
+        SecretId: 'CredencialesBD',
+        VersionStage: 'AWSCURRENT',
+      })
+    );
+
+    return JSON.parse(response.SecretString || '{}');
+  } catch (error) {
+    console.error('Error getting database secrets:', error);
+    throw new Error('Error getting database secrets');
+  }
+
+}
 
 @Module({
   imports: [
@@ -10,16 +31,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD')?.replace(/\\n/g, '\n'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const dbConfig = await getDatabaseSecrets();
+
+        return {
+          type: dbConfig.engine,
+          host: dbConfig.host,
+          port: Number(dbConfig.port),
+          username: dbConfig.username,
+          password: dbConfig.password?.replace(/\\n/g, '\n'),
+          database: dbConfig.database,
+          autoLoadEntities: true,
+          synchronize: true,
+          ssl: {
+            ca: fs.readFileSync('/app/rds-ca.pem').toString(),
+          }
+        };
+      },
     }),
   ],
 })
