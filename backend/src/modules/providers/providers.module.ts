9cf97b66a9b1df187ce8091af3d523b6b828c78a
andres0v13d0 <oviedojonathan2001@gmail.com>
2025-03-21 14:10:29 -0500

    Auth Module

--- backend/src/modules/providers/providers.module.ts
+++ backend/src/modules/providers/providers.module.ts
@@ -3,9 +3,10 @@ import { ProvidersController } from './providers.controller';
 import { ProvidersService } from './providers.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Provider } from './entity/provider.entity';
+import { User } from '../users/entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Provider])],
+  imports: [TypeOrmModule.forFeature([Provider, User])],
   controllers: [ProvidersController],
   providers: [ProvidersService],
   exports: [ProvidersService],
