8250c15ac611390fb1aab2ee61ec46a76f4b2371
andres0v13d0 <oviedojonathan2001@gmail.com>
2025-03-23 16:24:46 -0500

    Front

--- backend/src/modules/auth/dto/login.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class LoginDto {
-  @IsString()
-  @IsNotEmpty()
-  token: string;
-}
--- backend/src/modules/providers/entity/provider.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Provider } from './provider.entity';
-
-describe('Provider', () => {
-  it('should be defined', () => {
-    expect(new Provider()).toBeDefined();
-  });
-});
