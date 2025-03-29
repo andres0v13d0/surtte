b0bc5af90b3a58f68034f983697803c18432e44a
andres0v13d0 <oviedojonathan2001@gmail.com>
2025-03-28 19:44:24 -0500

    Update BarSup.js

--- frontend/src/components/BarSup.js
+++ frontend/src/components/BarSup.js
@@ -4,16 +4,16 @@ import './BarSup.css';
 const BarSup = () => {
     return (
         <div className='bar-sup'>
-            <a href="#" id='link-sup'>Opcion ssss sss </a>
-            <a href="#" id='link-sup'>Opcion 2</a>
-            <a href="#" id='link-sup'>Opcion 3</a>
-            <a href="#" id='link-sup'>Opcion 4</a>
-            <a href="#" id='link-sup'>Opcion 5</a>
-            <a href="#" id='link-sup'>Opcion 6</a>
-            <a href="#" id='link-sup'>Opcion 7</a>
-            <a href="#" id='link-sup'>Opcion 8</a>
-            <a href="#" id='link-sup'>Opcion 9</a>
-            <a href="#" id='link-sup'>Opcion 10</a>
+            <a href="/" id='link-sup'>Opcion ssss sss </a>
+            <a href="/" id='link-sup'>Opcion 2</a>
+            <a href="/" id='link-sup'>Opcion 3</a>
+            <a href="/" id='link-sup'>Opcion 4</a>
+            <a href="/" id='link-sup'>Opcion 5</a>
+            <a href="/" id='link-sup'>Opcion 6</a>
+            <a href="/" id='link-sup'>Opcion 7</a>
+            <a href="/" id='link-sup'>Opcion 8</a>
+            <a href="/" id='link-sup'>Opcion 9</a>
+            <a href="/" id='link-sup'>Opcion 10</a>
         </div>
     );
 }
