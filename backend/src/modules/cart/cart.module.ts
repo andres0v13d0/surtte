e673d22206ce9af63398f32c866e88782eb1e829
andres0v13d0 <oviedojonathan2001@gmail.com>
2025-04-24 11:43:50 -0500

    Update cart.module.ts

--- backend/src/modules/cart/cart.module.ts
+++ backend/src/modules/cart/cart.module.ts
@@ -4,10 +4,12 @@ import { CartItem } from './entity/cart.entity';
 import { Product } from '../products/entities/product.entity';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CartItem, Product]),
+    UsersModule
   ],
   controllers: [CartController],
   providers: [CartService],
