b73fe09b20a4f47c5cba719777d55b6f2dcff336
andres0v13d0 <oviedojonathan2001@gmail.com>
2025-04-28 15:21:33 -0500

    Update notification.module.ts

--- backend/src/modules/notification/notification.module.ts
+++ backend/src/modules/notification/notification.module.ts
@@ -4,11 +4,13 @@ import { NotificationEntity } from './entity/notification.entity';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { RedisModule } from '../redis/redis.module'; 
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([NotificationEntity]),
-    RedisModule, 
+    RedisModule,
+    UsersModule
   ],
   providers: [NotificationService],
   controllers: [NotificationController],
