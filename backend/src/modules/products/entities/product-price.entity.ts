823bbdda7b2d47dde25e8e4dcf971735b0086dda
Andres Oviedo <151101811+andres0v13d0@users.noreply.github.com>
2025-04-30 18:24:29 -0500

    Update product-price.entity.ts

--- backend/src/modules/products/entities/product-price.entity.ts
+++ backend/src/modules/products/entities/product-price.entity.ts
@@ -1,27 +1,37 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    CreateDateColumn,
-  } from 'typeorm';
-  import { Product } from './product.entity';
-  
-  @Entity('product_prices')
-  export class ProductPrice {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-  
-    @ManyToOne(() => Product, { nullable: false, onDelete: 'CASCADE' })
-    product: Product;
-  
-    @Column({ type: 'varchar', length: 100 })
-    minQuantity: string;
-  
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    pricePerUnit: number;
-  
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date;
-  }
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { Product } from './product.entity';
+
+@Entity('product_prices')
+export class ProductPrice {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Product, { nullable: false, onDelete: 'CASCADE' })
+  product: Product;
+
+  @Column({ type: 'int' })
+  minQuantity: number;
+
+  @Column({ type: 'int' })
+  maxQuantity: number;
+
+  @Column({ type: 'varchar', length: 20 })
+  unity: string; // 'unidad' o 'docena'
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  description: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
+
   
